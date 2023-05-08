62df83a4472376e35c6ed901a4f308472f504095
Joshua Williams <joshmackwilliams@protonmail.com>
2023-05-08 10:35:29 -0500

    Optimization, temporarily remove retraining and ndarray

--- src/integer.rs
+++ src/integer.rs
@@ -1,96 +1,139 @@
-use ndarray::parallel::prelude::*;
-use ndarray::prelude::*;
 use rand::{prelude::SliceRandom, Rng};
-
-type IntegerValue = i32;
+use rayon::prelude::*;
 
 pub struct IntegerHDModel {
-    quanta_vectors: Array2<bool>,
-    feature_vectors: Array2<bool>,
-    class_vectors: Array2<IntegerValue>,
+    // Binary-valued quanta vectors
+    quanta_vectors: Vec<u32>,
+    // Binary-valued feature vectors
+    feature_vectors: Vec<u32>,
+    // Integer-valued class vectors
+    class_vectors: Vec<i32>,
+    // Number of chunks in the model's feature vectors (actual dimensionality / 32)
+    model_dimensionality_chunks: usize,
+    // The actual model dimensionality
     model_dimensionality: usize,
+    // Number of features in each input example
     input_dimensionality: usize,
-    input_quanta: usize,
-    n_classes: usize,
+    // Number of values that each input feature can take on
+    //input_quanta: usize,
+    // Number of class vectors computed by the model
+    //n_classes: usize,
 }
 
 impl IntegerHDModel {
     pub fn new(
-        model_dimensionality: usize,
+        model_dimensionality_chunks: usize,
         input_dimensionality: usize,
         input_quanta: usize,
         n_classes: usize,
+        // RNG for model initialization
         rng: &mut impl Rng,
     ) -> Self {
-        let mut quanta_vectors: Array2<bool> = Array::default((input_quanta, model_dimensionality));
-        for i in 0..model_dimensionality {
-            quanta_vectors[(0, i)] = rng.gen();
-        }
+        // Compute the actual model dimensionality
+        let model_dimensionality = model_dimensionality_chunks * 32;
 
+        // Allocate space for quanta vectors
+        let mut quanta_vectors: Vec<u32> =
+            Vec::with_capacity(model_dimensionality_chunks * input_quanta);
+
+        // Randomly generate the first quantum vector
+        quanta_vectors.extend((0..model_dimensionality_chunks).map(|_| rng.gen::<u32>()));
+
+        // Randomly order some bits to flip
         let mut order_to_flip: Vec<usize> = (0..model_dimensionality).collect();
         order_to_flip.shuffle(rng);
         let flip_per_quantum = model_dimensionality / 2 / (input_quanta - 1);
 
+        // Make a vector for each quanta
         for quantum in 1..input_quanta {
-            for i in 0..model_dimensionality {
-                quanta_vectors[(quantum, i)] = quanta_vectors[(quantum - 1, i)];
+            // Some useful values
+            let previous_quantum = quantum - 1;
+            let quantum_offset_chunks = quantum * model_dimensionality_chunks;
+            let previous_quantum_offset_chunks = previous_quantum * model_dimensionality_chunks;
+
+            // Copy the previous quanta
+            for i in 0..model_dimensionality_chunks {
+                quanta_vectors.push(quanta_vectors[previous_quantum_offset_chunks + i]);
             }
+
+            // Flip some bits
             for i in 0..flip_per_quantum {
                 let index_to_flip = order_to_flip[i + (quantum - 1) * flip_per_quantum];
-                quanta_vectors[(quantum, index_to_flip)] ^= true;
+                let chunk_to_flip = index_to_flip / 32;
+                let bit_to_flip = index_to_flip % 32;
+                quanta_vectors[quantum_offset_chunks + chunk_to_flip] ^= 1 << bit_to_flip;
             }
         }
 
-        let feature_vectors: Array2<bool> =
-            Array::from_shape_simple_fn((input_dimensionality, model_dimensionality), || rng.gen());
+        // Allocate space for binary-valued feature vectors
+        let mut feature_vectors =
+            Vec::with_capacity(input_dimensionality * model_dimensionality_chunks);
+        feature_vectors.resize_with(input_dimensionality * model_dimensionality_chunks, || {
+            rng.gen::<u32>()
+        });
 
-        let class_vectors: Array2<IntegerValue> = Array::zeros((n_classes, model_dimensionality));
+        // Allocate space for integer-valued class vectors
+        let class_vectors = vec![0_i32; n_classes * model_dimensionality];
 
         IntegerHDModel {
             quanta_vectors,
             feature_vectors,
             class_vectors,
+            model_dimensionality_chunks,
             model_dimensionality,
             input_dimensionality,
-            input_quanta,
-            n_classes,
+            //input_quanta,
+            //n_classes,
         }
     }
 
-    pub fn encode(&self, input: &[Vec<usize>]) -> Array2<bool> {
-        let mut output: Array2<bool> =
-            Array::default((input.len(), self.model_dimensionality));
+    // Encode a batch of images
+    pub fn encode(&self, input: &[Vec<usize>]) -> Vec<u32> {
+        // Preallocate the output space
+        let mut output = vec![0_u32; input.len() * self.model_dimensionality_chunks];
+        // threshold = half the input dimensionality
+        // This serves as the threshold for the majority function that we use on images
+        let threshold = self.input_dimensionality as u32 >> 1;
         output
-            // For some reason, rows_mut doesn't work with parallelism, but this is equivalent
-            .axis_iter_mut(Axis(0))
-            .into_par_iter()
-            // Pair each output row with the corresponding image
+            // Compute one image vector at a time
+            .par_chunks_mut(self.model_dimensionality_chunks)
+            // Pair each vector with the corresponding image
             .zip(input.into_par_iter())
-            .for_each(|(mut output, image)| {
-                for position in 0..self.model_dimensionality {
-                    let mut count: usize = 0;
+            .for_each(|(output, image)| {
+                // We'll do this chunk-by-chunk - handling fixed-size chunks makes things easier
+                for chunk in 0..self.model_dimensionality_chunks {
+                    // Temporary stack-based scratch space for our majority function
+                    let mut counts: [u32; 32] = [0; 32];
+                    // Compute this chunk of the feature vector for each pixel
                     image.iter().enumerate().for_each(|(i, value)| {
-                        count += (self.feature_vectors[(i, position)]
-                            ^ self.quanta_vectors[(*value, position)]) as usize
+                        // The vector for a pixel is the position XOR the value
+                        let xored_chunk = self.feature_vectors
+                            [i * self.model_dimensionality_chunks + chunk]
+                            ^ self.quanta_vectors
+                                [*value * self.model_dimensionality_chunks + chunk];
+                        // Separate out each bit and count it
+                        count_bits_unsigned(xored_chunk, &mut counts);
                     });
-                    output[position] = count > image.len() / 2;
+                    // Condense our list of counts into a binarized bhunk and add it to the output
+                    output[chunk] = binarize_unsigned_chunk(&counts, threshold);
                 }
             });
         output
     }
 
-    pub fn train(&mut self, examples: ArrayView2<bool>, labels: ArrayView1<IntegerValue>) {
+    pub fn train(&mut self, examples: &[u32], labels: &[usize]) {
         examples
-            .rows()
-            .into_iter()
+            .chunks(self.model_dimensionality_chunks)
             .zip(labels.iter())
             .for_each(|(example, label)| {
-                self.class_vectors.slice_mut(s![*label as usize, ..]).iter_mut().zip(example.iter()).for_each(|(element, example)| {
-                    *element += (*example as IntegerValue) * 2 - 1;
-                });
+                for i in 0..self.model_dimensionality {
+                    self.class_vectors[i + (*label * self.model_dimensionality)] +=
+                        bit_as_i32(example, i);
+                }
             });
 
-        let mut epoch = 0;
+        /*
+        let mut epoch: usize = 0;
         let mut best = 0_usize;
         let mut epochs_since_improvement = 0;
         while epochs_since_improvement < 5 {
@@ -102,12 +145,8 @@ impl IntegerHDModel {
                 .for_each(|(example, label)| {
                     let predicted = self.classify(example);
                     if predicted != (*label as usize) {
-                        self.class_vectors.slice_mut(s![*label as usize, ..]).iter_mut().zip(example.iter()).for_each(|(element, example)| {
-                            *element += (*example as IntegerValue) * 2 - 1;
-                        });
-                        self.class_vectors.slice_mut(s![predicted as usize, ..]).iter_mut().zip(example.iter()).for_each(|(element, example)| {
-                            *element -= (*example as IntegerValue) * 2 - 1;
-                        });
+                        self.class_vectors.row_mut(*label).add_assign(&example);
+                        self.class_vectors.row_mut(predicted).sub_assign(&example);
                     } else {
                         correct += 1;
                     }
@@ -121,40 +160,99 @@ impl IntegerHDModel {
             }
             epoch += 1;
         }
+        */
     }
 
-    pub fn classify(&self, input: ArrayView1<bool>) -> usize {
+    pub fn classify(&self, input: &[u32]) -> usize {
         self.class_vectors
-            .rows()
-            .into_iter()
+            .chunks(self.model_dimensionality)
+            .map(|x| cosine_similarity(input, x))
             .enumerate()
-            .max_by_key(|(_, x)| cosine_similarity(input, *x))
+            .max_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
             .unwrap()
             .0
     }
 }
 
-fn magnitude(a: ArrayView1<i32>) -> f64 {
-    let square_sum: f64 = a
+// Majority function, operating on a list of counts and a threshold
+// - counts[i] holds the number of examples in which bit i was set
+// - threshold is set to half the number of examples
+// - returns the inverse result of applying the majority function
+fn binarize_unsigned_chunk(counts: &[u32], threshold: u32) -> u32 {
+    let mut output = 0;
+    for bit in 0..32 {
+        output |= ((counts[bit] < threshold) as u32) << bit;
+    }
+    output
+}
+
+// Given a binarized chunk, separate each bit out and add it to an array of counts
+fn count_bits_unsigned(chunk: u32, counts: &mut [u32]) {
+    for bit in 0..32 {
+        counts[bit] += chunk >> (31 - bit) & 1;
+    }
+}
+
+fn magnitude(a: &[i32]) -> f64 {
+    let square_sum: i64 = a
         .iter()
         .map(|x| {
-            let x: f64 = (*x).into();
+            let x = *x as i64;
             x * x
         })
         .sum();
-    square_sum.sqrt()
+    (square_sum as f64).sqrt()
 }
 
-fn dot(a: ArrayView1<bool>, b: ArrayView1<i32>) -> f64 {
-    a.iter()
-        .zip(b.iter())
-        .map(|(x, y)| ((*x as u8 as f64) * 2.0 - 1.0) * (*y as f64))
-        .sum()
+fn dot(binary_vec: &[u32], integer_vec: &[i32]) -> i32 {
+    let mut sum: i32 = 0;
+
+    // Iterate over the chunks
+    for (chunk_index, chunk) in binary_vec.iter().enumerate() {
+        let chunk_bit_offset = chunk_index << 5;
+        // Iterate over the bits
+        for bit_index in 0..32 {
+            // Grab the value of this feature from the integer vector
+            let feature_value = integer_vec[bit_index + chunk_bit_offset];
+            // If it needs to be negated, we'll do it manually
+            // Hopefully saves some time over converting to 1 or -1 and doing a multiplication
+            let xor_mask = ((*chunk as i32) << (31 - bit_index)) >> 31;
+            let add = xor_mask & 1;
+            sum += (feature_value ^ xor_mask) + add;
+        }
+    }
+
+    sum
 }
 
-pub fn cosine_similarity(
-    a: ArrayView1<bool>,
-    b: ArrayView1<i32>,
-) -> u32 {
-    ((dot(a, b) / magnitude(b)) * 1048576.0) as u32
+pub fn cosine_similarity(a: &[u32], b: &[i32]) -> f64 {
+    (dot(a, b) as f64) / magnitude(b)
+}
+
+// Utility function to get a bit from a vector as -1 or 1
+fn bit_as_i32(chunks: &[u32], bit_index: usize) -> i32 {
+    let chunk = chunks[bit_index >> 5];
+    let bit = bit_index & 31;
+    ((chunk as i32) << (31 - bit) >> 31) | 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bit_as_i32() {
+        let v = [0b10101010101010101010101010101010_u32];
+        assert_eq!(bit_as_i32(&v, 3), -1);
+        assert_eq!(bit_as_i32(&v, 4), 1);
+        assert_eq!(bit_as_i32(&v, 0), 1);
+    }
+
+    #[test]
+    pub fn test_dot() {
+        let mut v = [0; 32];
+        v[0..8].copy_from_slice(&[1, 2, -3, -4, 5, 6, -7, -8]);
+        let w = [0b10110000];
+        assert_eq!(dot(&w, &v), 1 + 2 - 3 - 4 - 5 - 6 - 7 + 8);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,85 @@
-// Ndarray is used throughout the project to avoid using vectors for everything
-use ndarray::{s, Array1, Array2, ArrayView1};
-
 // For initializing random feature vectors
 use rand::{rngs::SmallRng, SeedableRng};
 
 // Needed to write partial lines to the console
 use std::io::{self, Write};
+use std::time::Instant;
 
-use hd_vsa_mnist::binary::{classify, majority, make_feature_vectors};
 use hd_vsa_mnist::integer::IntegerHDModel;
-use hd_vsa_mnist::mnist::{encode_images, load_mnist};
-use hd_vsa_mnist::Chunk;
+use hd_vsa_mnist::mnist::load_mnist;
 
 fn main() {
     let train_filename = "mnist_train.csv";
     let test_filename = "mnist_test.csv";
-    let n_chunks = 5000;
+    let n_chunks = 64; // Dimensionality of the model / 32
     let mut rng = SmallRng::seed_from_u64(0);
 
     // Load the dataset - raw images and labels, no vectorization
     print!("Loading data... ");
     let _ = io::stdout().flush();
-    let (train_images, train_labels) = load_mnist(train_filename);
+    let now = Instant::now();
+    let (train_images, train_y) = load_mnist(train_filename);
     println!(
-        "Loaded {} examples from {}",
+        "Loaded {} examples from {} [{}ms]",
         train_images.len(),
-        train_filename
+        train_filename,
+        now.elapsed().as_millis()
     );
 
-    assert!(!train_images.is_empty(), "No training images found");
+    assert!(!train_images.is_empty(), "No training images found!");
     let image_area = train_images[0].len();
 
-    /*
-    // Set up the feature vectors
-    // For now, we use two levels. This will be more sophisticated in the future
-    print!("Generating feature vectors... ");
+    // Set up the model, including feature vectors and quantum vectors
+    print!("Initializing model... ");
     let _ = io::stdout().flush();
-    let feature_vecs = make_feature_vectors(image_area, n_chunks, &mut rng);
-    println!("Done");
-
-    // Use feature vectors to encode the images
-    print!("Encoding images using feature vectors... ");
-    let _ = io::stdout().flush();
-    let train_x = encode_images(train_images, feature_vecs.view(), n_chunks);
-    let train_y = Array1::from(train_labels);
-    println!("Done");
-
-    print!("Computing class vectors from examples... ");
-    let _ = io::stdout().flush();
-
-    // Sort the examples by label
-    let mut class_examples: Vec<Vec<ArrayView1<Chunk>>> = Vec::new();
-    class_examples.resize_with(10, Vec::new);
-    for i in 0..train_x.shape()[0] {
-        class_examples[train_y[i]].push(train_x.slice(s![i, ..]));
-    }
-
-    // Compute class vectors
-    let mut class_vectors = Array2::<Chunk>::zeros((10, n_chunks));
-    for (class, examples) in class_examples.iter().enumerate() {
-        majority(examples, n_chunks, class_vectors.slice_mut(s![class, ..]));
-    }
-    println!("Done");
-    */
-
+    let now = Instant::now();
     let mut model = IntegerHDModel::new(n_chunks, image_area, 256, 10, &mut rng);
+    println!("Done [{}ms]", now.elapsed().as_millis());
+
+    // Encode the training images using the model
     print!("Encoding images using feature vectors... ");
     let _ = io::stdout().flush();
+    let now = Instant::now();
     let train_x = model.encode(&train_images);
-    println!("Done");
-    let train_labels: Vec<i32> = train_labels.into_iter().map(|x| x as i32).collect();
-    let train_y = Array1::from(train_labels);
-    model.train(train_x.view(), train_y.view());
+    println!("Done [{}ms]", now.elapsed().as_millis());
+
+    // Train the model
+    print!("Training model... ");
+    let _ = io::stdout().flush();
+    let now = Instant::now();
+    model.train(&train_x, &train_y);
+    println!("Done [{}ms]", now.elapsed().as_millis());
 
     // Load the test data
     print!("Loading test data... ");
     let _ = io::stdout().flush();
-    let (test_images, test_labels) = load_mnist(test_filename);
+    let now = Instant::now();
+    let (test_images, test_y) = load_mnist(test_filename);
     println!(
-        "Loaded {} examples from {}",
+        "Loaded {} examples from {} [{}ms]",
         test_images.len(),
-        test_filename
+        test_filename,
+        now.elapsed().as_millis()
     );
 
-    /*
-    // Encode the test data as we did with the training data
+    // Encode the test data
     print!("Encoding test data... ");
     let _ = io::stdout().flush();
-    let test_x = encode_images(test_images, feature_vecs.view(), n_chunks);
-    let test_y = Array1::from(test_labels);
-    println!("Done");
-    */
-
+    let now = Instant::now();
     let test_x = model.encode(&test_images);
-    let test_y = Array1::from(test_labels);
+    println!("Done [{}ms]", now.elapsed().as_millis());
 
     // Classify the test data and compute accuracy
     print!("Testing model... ");
     let _ = io::stdout().flush();
+    let now = Instant::now();
     let mut correct = 0;
-    for i in 0..test_x.shape()[0] {
-        let x = test_x.slice(s![i, ..]);
-        let y = &test_y[i];
+    for (x, y) in test_x.chunks(test_x.len() / test_images.len()).zip(test_y) {
         let class = model.classify(x);
-        if class == *y as usize {
+        if class == y {
             correct += 1;
         }
     }
-
-    let acc = correct as f64 / test_x.shape()[0] as f64;
-    println!("Done - Accuracy = {}", acc);
+    let acc = correct as f64 / test_images.len() as f64;
+    println!("Done - Accuracy = {} [{}ms]", acc, now.elapsed().as_millis());
 }
