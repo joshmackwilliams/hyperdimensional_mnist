dca1b29ca0a163c7300ed0a25da97e892fb00228
Joshua Williams <joshmackwilliams@protonmail.com>
2023-05-10 09:55:57 -0500

    Clean up type definitions

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod mnist;
 pub mod hd_model;
 pub mod counting_binary_vector;
 
-pub type BinaryChunk = std::simd::usizex8;
 pub type ChunkElement = usize;
 pub const CHUNK_ELEMENTS: usize = 8;
-pub const CHUNK_SIZE: usize = ChunkElement::BITS as usize * CHUNK_ELEMENTS;
+pub type BinaryChunk = std::simd::Simd<ChunkElement, CHUNK_ELEMENTS>;
+pub const CHUNK_SIZE: usize = std::mem::size_of::<BinaryChunk>() * 8; // Multiply by 8 to get number of bits
